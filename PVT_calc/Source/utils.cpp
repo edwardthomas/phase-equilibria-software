ab7a092662755df35b5de74a0487a93ce5f3d5e4
heath <heath_henley@my.uri.edu>
2015-11-23 18:45:11 -0500

    added string trimming utility to clean whitespace from keywords in input file

---
+++ PVT_calc/Source/utils.cpp
@@ -0,0 +1,14 @@
+#include "utils.hpp" 
+
+std::string trim(const std::string &s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && isspace(*it))
+        it++;
+
+    std::string::const_reverse_iterator rit = s.rbegin();
+    while (rit.base() != it && isspace(*rit))
+        rit++;
+
+    return std::string(it, rit.base());
+}
---
+++ PVT_calc/Source/utils.hpp
@@ -0,0 +1,12 @@
+#ifndef __UTILS__
+#define __UTILS__
+
+#include <string> 
+using namespace std; 
+
+
+std::string trim(const std::string &s);
+
+
+
+#endif 

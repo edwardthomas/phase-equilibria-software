0fec2297936ac071c274f307c28be3e1e9f34fc7
Robert B. Kelly <robert_kelly@uri.edu>
2015-11-23 17:30:59 -0500

    number of components in library changed to 17

--- PVT_calc/Source/global.h
+++ PVT_calc/Source/global.h
@@ -1,7 +1,7 @@
 #pragma once
 namespace GLOBAL
 {
-	const int MAX_LIB_COMPS = 2; //< number of components in build in component database
+	const int MAX_LIB_COMPS = 17; //< number of components in build in component database
 	const double R = 83.144598; //< gas constant 
 	extern int NC; //< number of components in the system
 	extern int NP; //< max number of equilibrium phases

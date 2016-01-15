259acce5535a19050594c21d939a667170677253
Robert B. Kelly <robert_kelly@uri.edu>
2016-01-14 22:12:29 -0500

    Update VDW_EquationOfState.h

--- PVT_calc/Source/VDW_EquationOfState.h
+++ PVT_calc/Source/VDW_EquationOfState.h
@@ -1,1 +1,14 @@
+#include "EquationOfState.h"
+
+
+#pragma once
+class VDW_EquationOfState : public EquationOfState
+{
+public:
+	VDW_EquationOfState(void);
+	~VDW_EquationOfState(void);
+	void computeMixParameters(vector<double> zc, ComponentLib *p, double Pres, double Temp);
+	void computeDensity(vector<double> zc, ComponentLib *p,double Pres, double Temp, int phase_id, vector<double> &den);
+	void computeFugacity(vector<double> zc, ComponentLib *p,double Pres, double Temp, int phase_id, vector<double> &fug);
+};
 
